ec5a84677d0d70f4f2c3cec8066373d888d9d8b4
mrcabo <dcgdiego@gmail.com>
2021-05-18 23:16:03 -0400

    Starting train model

--- notebooks/dev_make_datasets.py
+++ notebooks/dev_make_datasets.py
@@ -1,13 +1,12 @@
 # %%
-from dask_ml.datasets import make_regression
 from dask_ml.model_selection import train_test_split
 from dask_ml.preprocessing import Categorizer, DummyEncoder, StandardScaler
 import numpy as np
 import dask.array as da
 import dask.dataframe as dd
-from dask.distributed import Client, progress
 import dask as d
 import pandas as pd
+from pathlib import Path
 
 
 def chunk(s):
@@ -26,18 +25,33 @@ def finalize(s):
     )
 
 
-#### Remove NaNs ####
+def _save_datasets(train, test, outdir: Path):
+    """Save data sets into nice directory structure and write SUCCESS flag."""
+    out_train = outdir / 'train.parquet/'
+    out_test = outdir / 'test.parquet/'
+    flag = outdir / '.SUCCESS'
+
+    train.to_parquet(str(out_train))
+    test.to_parquet(str(out_test))
+
+    flag.touch()
+
+
 # %%
 in_csv = '/home/diego/Coding/code-challenge-2020/data_root/raw/wine_dataset.csv'
+out_dir = Path('/home/diego/Coding/code-challenge-2020/data_root/processed')
+out_dir.mkdir(parents=True, exist_ok=True)
+
 # client = Client(n_workers=2, threads_per_worker=2, memory_limit='1GB')
 ddf = dd.read_csv(in_csv, blocksize=1e6)
+# we set the index so we can properly execute loc below
 ddf = ddf.set_index('Unnamed: 0')
 
 ddf['country'] = ddf['country'].fillna('Unknown')
 ddf['province'] = ddf['province'].fillna('Unknown')
 ddf['taster_name'] = ddf['taster_name'].fillna('Unknown')
-
-# fill missing values using the 'province' column. Most common value for each province will be used.rest are labeled as Unknown.
+# Fill region_1 missing values using the 'province' column.
+# Most common value for each province will be used. Rest are labeled Unknown
 mode = dd.Aggregation('mode', chunk, agg, finalize)
 most_common_region = ddf.groupby(
     ['province']).agg({'region_1': mode}).compute()
@@ -45,6 +59,8 @@ ddf['region_1'] = ddf.apply(lambda x: most_common_region.loc[x.province, 'region
                             if x.province in most_common_region['region_1'].index
                             else 'Unknown', axis=1).where(ddf['region_1'].isna(), ddf['region_1'])
 
+# We fill price values with the province's average price. If that is
+# not available, we use the global average price
 mean_prices = ddf.groupby(['province'])['price'].mean().compute()
 global_mean = ddf['price'].mean().compute()
 mean_prices = mean_prices.fillna(global_mean)
@@ -52,32 +68,32 @@ ddf['price'] = ddf.apply(lambda x: mean_prices[x['province']], axis=1, meta=(
     'x', 'f8')).where(ddf['price'].isna(), ddf['price'])
 ddf = ddf.drop(['description', 'designation', 'region_2',
                 'taster_twitter_handle', 'title'], axis=1)
-ddf.head()
-
-
-# %%
-# Useful if we use sklearn pipelines
-# ddf.dtypes
-# ce = Categorizer()
-# ce.fit_transform(ddf).dtypes
-
-# ddf.categorize(columns=['country', 'province', 'region_1'])
-ddf = ddf.categorize()
-
-# # %%
-# scaler = StandardScaler()
-# ddf['price'] = scaler.fit_transform(ddf[['price']]).price
-
-# # %%
+# Encode categorical values using one-hot encoding.
+# This results in >6k columns. Maybe we'll need to change the encoding type
+# for some features such as 'winery' with so many unique values.
+# Also, I think this should be done in the model task.
+# ddf = ddf.categorize()
 # encoder = DummyEncoder()
 # ddf = encoder.fit_transform(ddf)
 
 # %%
-train, test = ddf.random_split([0.9, 0.1], shuffle=True)
+# # Normalize price values
+# scaler = StandardScaler()
+# ddf['price'] = scaler.fit_transform(ddf[['price']]).price
 
-# y = ddf[['points']]
-# X = ddf.drop('points', axis=1)
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, shuffle=True)
 # %%
-train.head()
+ddf.head()
+
+# n_samples = len(ddf)
+# idx = np.arange(n_samples)
+# test_idx = idx[:n_samples // 10]
+# test = ddf.loc[test_idx]
+
+# train_idx = idx[n_samples // 10:]
+# train = ddf.loc[train_idx]
+
 # %%
+# train, test = ddf.random_split([0.9, 0.1], shuffle=True)
+train, test = ddf.random_split([0.9, 0.1])
+
+_save_datasets(train, test, out_dir)
---
+++ notebooks/dev_train_model.py
@@ -0,0 +1,65 @@
+
+# %%
+import logging
+from operator import length_hint
+from pathlib import Path
+from distributed import Client
+import numpy as np
+import dask.dataframe as dd
+import click
+from sklearn.pipeline import make_pipeline
+from sklearn.compose import make_column_transformer
+# from make_dataset.dataset import chunk, agg, finalize, _save_datasets
+from dask_ml.preprocessing import Categorizer, StandardScaler, DummyEncoder
+# from sklearn.preprocessing import StandardScaler, OneHotEncoder, OrdinalEncoder, LabelEncoder
+# from dask_ml.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
+from sklearn import set_config
+
+
+ddf = dd.read_parquet('/home/diego/Coding/code-challenge-2020/data_root/processed/train.parquet',
+                      engine='pyarrow')
+
+
+# %%
+X, y = ddf.drop(['points'], axis=1), ddf[['points']]
+X.head()
+
+# # %%
+# scaler = StandardScaler()
+# enc = DummyEncoder()
+# cat = Categorizer()
+# df2 = ddf
+# df2['price'] = scaler.fit_transform(df2[['price']]).price
+# df2 = cat.fit_transform(df2)
+# df2 = enc.fit_transform(df2)
+# df2.head()
+# df2_X, df2_y = df2.drop(['points'], axis=1), df2[['points']]
+# model = LinearRegression()
+
+# %%
+# Prepare data
+set_config(display='diagram')
+num_proc = make_pipeline(StandardScaler())
+cat_proc = make_pipeline(Categorizer(), DummyEncoder())
+cat_cols = X.columns.to_list()
+cat_cols.remove('price')
+preprocessor = make_column_transformer((num_proc, ['price']),
+                                       (cat_proc, cat_cols))
+model = make_pipeline(preprocessor, LinearRegression())
+model
+
+# %%
+model.fit(X, y)
+
+# %%
+cross_val_scores = cross_val_score(model, X.compute(), y.compute(), cv=5)
+# cross_val_scores = cross_val_score(model, X, y, cv=5)  # Using dask dataframes
+
+# sklearn cross_val_score doesn't work with dask dataframes. I could create a
+# simple k-fold cross validation for loop but lets leave it unless I manage to
+# make docker-compose work properly
+
+#%%
+model.score(X,y)
